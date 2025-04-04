4e68d6c705129ca6da22a322e13413f90b756279
AlbertoSanPietro <albertoxsp@gmail.com>
2025-04-04 14:50:28 +0200

    Created FrancoCoin project using Intellij and Maven

---
+++ FrancoCoin/FrancoCoin/src/main/java/it/tff/francocoin/FrancoCoinApplication.java
@@ -0,0 +1,13 @@
+package it.tff.francocoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancoCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrancoCoinApplication.class, args);
+    }
+
+}
---
+++ FrancoCoin/FrancoCoin/src/test/java/it/tff/francocoin/FrancoCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tff.francocoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancoCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
